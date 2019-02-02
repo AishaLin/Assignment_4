0e136f25d3df6f99ac99cb5df09228fc6a4b754b
Aisha Lin <aishalin314@gmail..com>
2019-02-02 21:21:15 +0800

    week4

--- src/App.js
+++ src/App.js
@@ -1,28 +1,133 @@
+/*==============================================================
+  Assignment 3: Re-Implement Layout by React (Front-End Only)
+==============================================================*/
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './star.svg';
+import iconMenu from './icon_menu.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header/>
+        <Banner/>
+        <MainContent/>
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="main-header">
+        <div className="main-title">
+          <h1 className="title">Website Title /</h1>
+          <img src={logo} className="App-logo" alt="starlogo" />
+          <button className="menu"><img src={iconMenu} alt="menu trigger icon" /></button>
+        </div>
+        <ul className="main-nav">
+          <li className="close-icon"><a href="javascript:void(0)" className="closebtn">&times;</a></li>
+          <li><a href="#">Item1</a></li>
+          <li><a href="#">Item2</a></li>
+          <li><a href="#">Item3</a></li>
+          <li><a href="#">Item4</a></li>
+        </ul>
+      </header>
+
+    );
+  }
+}
+
+class Banner extends Component {
+  render() {
+    return (
+      <div className="banner">
+        <h1 className="headline">Welcome Message</h1>
+      </div>
+    );
+  }
+}
+
+class MainContent extends Component {
+  render() {
+    return (
+      <div>
+        <div className="section-title">
+          <h2>Section Title</h2>
+        </div>
+
+        <div className="content">
+          <Box/><Box/><Box/><Box/>
+        </div>
+
+        <footer className="mainFooter">
+          <span>Call to Action</span>
+        </footer>
+
+        <div className="content hide">
+          <Box/><Box/><Box/><Box/>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Box extends Component {
+  render() {
+    return (
+      <div className="box">
+        <h3>Content Box</h3>
       </div>
     );
   }
 }
 
 export default App;
+
+/*==============================================================
+  Assignment 1: Callback Function
+==============================================================*/
+// function delayedResult(n1, n2, delayTime, callback){
+//   let result = n1 + n2;
+//   setTimeout(callback, delayTime, result);
+// }
+//
+// delayedResult(4, 5, 3000, function(result){
+//   console.log(result);
+// });
+//
+// delayedResult(-5, 10, 2000, function(result){
+//   window.alert(result);
+// });
+
+/*==============================================================
+  Assignment 2: Callback Function and Advanced HTML DOM
+==============================================================*/
+// function ajax(src, callback){
+//   let xhr = new XMLHttpRequest();
+//   xhr.onreadystatechange = function (){
+//     const products = JSON.parse(xhr.responseText);
+//     let productInformation ="";
+//     if(xhr.readyState == 4 && xhr.status === 200){
+//       for(let i = 0; i < 3; i += 1){
+//         productInformation += `<li>name : ${products[i].name}</li>`;
+//         productInformation += `<li>price : ${products[i].price}</li>`;
+//         productInformation += `<li>description : ${products[i].description}</li>`;
+//       callback(productInformation);
+//       }
+//     }
+//   };
+//   xhr.open('GET', 'src');
+//   xhr.send();
+// }
+// function render(data){
+//   let p = document.querySelector('.productList');
+//   let ul = document.createElement('ul');
+//   ul.innerHTML = data;​
+//   p.appendChild(ul);​
+// }
+// ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){
+//   render(response);
+// });
--- src/index.js
+++ src/index.js
@@ -6,7 +6,34 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+/* ==========================================
+  Assignment 4-1: Click to Change Text.
+============================================= */
+const headline = document.querySelector('.headline');
+headline.addEventListener('click', () => {
+  headline.textContent = "Have a Good Time!";
+})
+
+/* ==========================================
+  Assignment 4-2: Click to Show/Close Menu.
+============================================= */
+const openNav = document.querySelector('.main-nav')
+const menuIcon = document.querySelector('.menu')
+const closeIcon = document.querySelector('.closebtn')
+
+menuIcon.addEventListener('click', () =>{
+  openNav.style.width = '100px';
+})
+closeIcon.addEventListener('click', () =>{
+  openNav.style.width = '0px';
+})
+
+/* ==========================================
+  Assignment 4-3: Click to Show More Content Boxes.
+============================================= */
+const mainFooter = document.querySelector('.mainFooter');
+const hide = document.querySelector('.hide');
+mainFooter.addEventListener('click', () => {
+  hide.style.display = "flex";
+})
 serviceWorker.unregister();

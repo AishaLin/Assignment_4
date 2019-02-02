11ce5f2627749c6e21a90e36c638dbadc4a7170c
Aisha Lin <aishalin314@gmail..com>
2019-02-02 20:46:35 +0800

    Updates

---
+++ precache-manifest.1d3b94d26a26a94544b58493d3efc61f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a57b2bdfdd66888c5c47",
+    "url": "/Assignment_4/static/css/main.9053de01.chunk.css"
+  },
+  {
+    "revision": "a57b2bdfdd66888c5c47",
+    "url": "/Assignment_4/static/js/main.a57b2bdf.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Assignment_4/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "3c74154d8251f8b9b55c",
+    "url": "/Assignment_4/static/js/runtime~main.3c74154d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Assignment_4/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0526e610338544ce5fc42406ac8ea1f8",
+    "url": "/Assignment_4/index.html"
+  }
+];
